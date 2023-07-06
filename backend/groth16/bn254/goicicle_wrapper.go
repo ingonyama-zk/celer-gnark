231a789186e734e2daeb2c52c6d1e5c0b44be1d5
Jeremy Felder <jeremy.felder1@gmail.com>
2023-07-06 07:12:36 +0300

    cleanup icicle wrapper

--- backend/groth16/bn254/goicicle_wrapper.go
+++ backend/groth16/bn254/goicicle_wrapper.go
@@ -1,113 +1,14 @@
 package groth16
 
 import (
-	"runtime"
-	"sync"
 	"unsafe"
 	"fmt"
 
 	curve "github.com/consensys/gnark-crypto/ecc/bn254"
 	"github.com/consensys/gnark-crypto/ecc/bn254/fr"
-	"github.com/consensys/gnark-crypto/ecc/bn254/fr/fft"
 	icicle "github.com/ingonyama-zk/icicle/goicicle/curves/bn254"
 	cudawrapper "github.com/ingonyama-zk/icicle/goicicle"
 )
-
-// Execute process in parallel the work function
-func Execute(nbIterations int, work func(int, int), maxCpus ...int) {
-
-	nbTasks := runtime.NumCPU()
-	if len(maxCpus) == 1 {
-		nbTasks = maxCpus[0]
-		if nbTasks < 1 {
-			nbTasks = 1
-		} else if nbTasks > 512 {
-			nbTasks = 512
-		}
-	}
-
-	if nbTasks == 1 {
-		// no go routines
-		work(0, nbIterations)
-		return
-	}
-
-	nbIterationsPerCpus := nbIterations / nbTasks
-
-	// more CPUs than tasks: a CPU will work on exactly one iteration
-	if nbIterationsPerCpus < 1 {
-		nbIterationsPerCpus = 1
-		nbTasks = nbIterations
-	}
-
-	var wg sync.WaitGroup
-
-	extraTasks := nbIterations - (nbTasks * nbIterationsPerCpus)
-	extraTasksOffset := 0
-
-	for i := 0; i < nbTasks; i++ {
-		wg.Add(1)
-		_start := i*nbIterationsPerCpus + extraTasksOffset
-		_end := _start + nbIterationsPerCpus
-		if extraTasks > 0 {
-			_end++
-			extraTasks--
-			extraTasksOffset++
-		}
-		go func() {
-			work(_start, _end)
-			wg.Done()
-		}()
-	}
-
-	wg.Wait()
-}
-
-/*
-* Adapters - these adapters convert between icicle and gnark
-* todo: add conditional rendering
- */
-
-func NttBN254GnarkAdapter(domain *fft.Domain, coset bool, scalars []fr.Element, isInverse bool, decimation int, deviceId int) []fr.Element {
-	if coset && !isInverse {
-		scale := func(cosetTable []fr.Element) {
-			Execute(len(scalars), func(start, end int) {
-				for i := start; i < end; i++ {
-					scalars[i].Mul(&scalars[i], &cosetTable[i])
-				}
-			}, runtime.NumCPU())
-		}
-		if decimation == icicle.DIT {
-			scale(domain.CosetTableReversed)
-		} else {
-			scale(domain.CosetTable)
-		}
-	}
-
-	nttResult := icicle.BatchConvertFromFrGnark[icicle.ScalarField](scalars)
-	icicle.NttBN254(&nttResult, isInverse, decimation, deviceId)
-
-	if coset && isInverse {
-		res := icicle.BatchConvertToFrGnark[icicle.ScalarField](nttResult)
-
-		scale := func(cosetTable []fr.Element) {
-			Execute(len(res), func(start, end int) {
-				for i := start; i < end; i++ {
-					res[i].Mul(&res[i], &cosetTable[i])
-				}
-			}, runtime.NumCPU())
-		}
-		if decimation == icicle.DIT {
-			scale(domain.CosetTableInv)
-		} else {
-			scale(domain.CosetTableInvReversed)
-		}
-
-		return res
-	}
-
-	return icicle.BatchConvertToFrGnark[icicle.ScalarField](nttResult)
-}
 
 func INttOnDevice(scalars []fr.Element, twiddles_d, cosetPowers_d unsafe.Pointer, size, sizeBytes int, isCoset bool) (unsafe.Pointer, unsafe.Pointer) {
 	scalars_d, _ := cudawrapper.CudaMalloc(sizeBytes)
